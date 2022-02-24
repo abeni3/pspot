a9690ab43e4d10533cf502cbbb2cfa6dada95e86
abeni3 <abenitamene2023@u.northwestern.edu>
2022-02-24 11:49:06 -0600

    ground work for functional components Spot

--- src/App.js
+++ src/App.js
@@ -1,25 +1,138 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import {
+  withGoogleMap,
+  withScriptjs,
+  GoogleMap,
+  Marker,
+  InfoWindow
+} from "react-google-maps";
+import { useData, useUserState, signInWithG, signOutOfG } from "./utilities/firebase.js";
 
-function App() {
+import scanSVG from "../src/styles/svgs/scan.svg";
+import accSVG from "../src/styles/svgs/account.svg";
+import helpSVG from "../src/styles/svgs/help.svg";
+import topLogo from "../src/styles/svgs/SpotLogos.png";
+import close from "../src/styles/svgs/close.svg";
+import CurrentLocationIcon from "../src/styles/svgs/Location.svg";
+import paws from "../src/styles/svgs/paws.png";
+import Activepaws from "../src/styles/svgs/ActivePaws.png";
+
+// const ldata = require('./data/stations.json');
+
+
+
+/* HELP COMMENT
+withGoogleMap initializes the map component while withScriptjs loads the Google Map JavaScript API v3.
+
+withScriptjs requires -
+  googleMapURL: String (Google Map API Key)
+  loadingElement: ReactElement
+
+withGoogleMap requires -
+  containerElement: ReactElement (needs both height & width properties) without which the map won't display.
+  mapElement: ReactElement
+
+#the methods basically load libraries that higher order functions that make the map work
+*/
+
+
+const SignInButton = () => (
+  <button className="btn"
+    onClick={() => signInWithG()}>
+    Sign In
+  </button>
+
+);
+
+const SignOutButton = () => (
+  <button className="btn"
+      onClick={() => signOutOfG()}>
+    Sign Out
+  </button>
+);
+
+
+
+export default function App() { 
+
+
+  const user = useUserState();
+  const [mdata, loading, error] =  useData("/");
+  if (error) return <h1>{error}</h1>;
+  if (loading) return <h1>Loading the data...</h1>;
+
+  // mdata.Locations.map(loc => (console.log(loc.name)));
+
+  function  Map()  {
+    
+    const [selectedStation, setSelectedStation] = useState(null);
+    
+    return (
+      <GoogleMap
+        defaultZoom={13}
+        defaultCenter={{ lat: 42.0565, lng: -87.6753 }} 
+        // defaultOptions={{ styles: mapStyles }} for a prettier map experience.
+      >
+        {mdata.Locations.map(station => (
+          <Marker
+            key={station.id}
+            position={{
+              lat: station.latitude,
+              lng: station.longitude
+            }}
+            onClick={() => {
+              setSelectedStation(station);
+            }}
+            icon={{
+              url: paws,
+              scaledSize: new window.google.maps.Size(25, 25)
+            }}
+          />
+        ))}
+  
+        {selectedStation && (
+          <InfoWindow
+            onCloseClick={() => {
+              setSelectedStation(null);
+            }}
+            position={{
+              lat: selectedStation.latitude,
+              lng: selectedStation.longitude
+            }}
+          >
+            <div>
+              <h2>{selectedStation.name}</h2>
+              <p>{selectedStation.amenities}</p>
+            </div>
+          </InfoWindow>
+        )}
+      </GoogleMap>
+    );
+  }
+
+  const MapWrapped = withScriptjs(withGoogleMap(Map));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div>
+      {user ? 
+     
+        <div style={{ width: "100vw", height: "100vh" }}>
+          
+            <MapWrapped
+              googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyC8bntpT3xn96QvTL2tp0q3z0PNr0A0QL4"
+              loadingElement={<div style={{ height: `10%` ,width: '100%'}} />}
+              containerElement={<div style={{ height: `80%`, width: '100%' }} />}
+              mapElement={<div style={{ height: `100%`,width: '100%' }} />}
+            />
+
+          <SignOutButton/>
+
+          
+        </div>
+    :
+    <SignInButton />
+    }
     </div>
   );
 }
-
-export default App;
---
+++ src/utilities/firebase.js
@@ -0,0 +1,65 @@
+import { initializeApp } from 'firebase/app';
+import { getDatabase, onValue, ref, set } from 'firebase/database';
+import { useState, useEffect } from 'react';
+import { getAuth, GoogleAuthProvider, onIdTokenChanged, signInWithPopup, signOut } from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBAAAaqRWLP7A4BwEmCVC2OWoKXw8j79W4",
+  authDomain: "dirtylittlepaws-487d9.firebaseapp.com",
+  databaseURL: "https://dirtylittlepaws-487d9-default-rtdb.firebaseio.com",
+  projectId: "dirtylittlepaws-487d9",
+  storageBucket: "dirtylittlepaws-487d9.appspot.com",
+  messagingSenderId: "781498327165",
+  appId: "1:781498327165:web:9da20c4afa727b8e5e5114"
+};
+
+const firebase = initializeApp(firebaseConfig);
+const database = getDatabase(firebase);
+
+
+
+export const useData = (path, transform) => {
+    const [data, setData] = useState();
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState();
+  
+    useEffect(() => {
+      const dbRef = ref(database, path);
+      const devMode = !process.env.NODE_ENV || process.env.NODE_ENV === 'development';
+      // if (devMode) { console.log(`loading ${path}`); }
+      return onValue(dbRef, (snapshot) => {
+        const val = snapshot.val();
+        if (devMode) { console.log(val); }
+        setData(transform ? transform(val) : val);
+        setLoading(false);
+        setError(null);
+      }, (error) => {
+        setData(null);
+        setLoading(false);
+        setError(error);
+      });
+    }, [path, transform]);
+    
+    return [data, loading, error];
+  };
+
+  export const useUserState = () => {
+    const [user, setUser] = useState();
+  
+    useEffect(() => {
+      onIdTokenChanged(getAuth(firebase), setUser);
+    }, []); 
+
+    if(user){
+      console.log(user.displayName)
+    }
+      
+    return user;
+  };
+  
+  export const signInWithG = () => {
+    signInWithPopup(getAuth(firebase), new GoogleAuthProvider());
+  };
+
+  export const signOutOfG = () => signOut(getAuth(firebase));
+
